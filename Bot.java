3b867258b43da6c7beac71ef7ea054f05553f894
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-13 13:52:53 -0500

    f

---
+++ Bot.java
@@ -0,0 +1,15 @@
+package me.marcuzzo.JDABot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Bot {
+
+	public void main (String[] args) throws Exception{
+		
+		String token = "NzMyMjkwMTUzNjg2NDk5MzY4.Xwycjg.GmD_c9soeD9t7GgGdl9n8iQ8yK0";
+		JDA jda = JDABuilder.createDefault(token).build();
+		
+		
+	}
+}
