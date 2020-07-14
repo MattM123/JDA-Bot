08aa21a7829bd152dfc0dace195afa79734bb63e
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-13 22:36:04 -0500

    g

--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -6,19 +6,20 @@ import net.dv8tion.jda.api.JDABuilder;
 public class Bot {
 
 	public static void main (String[] args) {
-		
 
 		int port = Integer.parseInt(System.getenv("PORT"));
 		String host = "0.0.0.0";
 		
-		
 		String token = "NzMyMjkwMTUzNjg2NDk5MzY4.Xw0Srg.Y6xwY481QrIGqcbHpv2fF4a5ZxY";
 		try {
 			JDA jda = JDABuilder.createDefault(token).build();
+			jda.addEventListener(port);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		
 		
 		
 	}
