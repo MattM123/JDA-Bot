d89a19f0cd827e02b7ad385938a414441f970275
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-16 20:27:17 -0500

    r

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -Dserver.port=$PORT -cp target/classes:target/dependency/* com.marcuzzo.JDABot.Bot
+worker: java $JAVA_OPTS -Dserver.port=$PORT -cp target/classes:target/dependency/* com.marcuzzo.JDABot.Bot
--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -14,6 +14,7 @@ public class Bot {
 		String token = "NzMyMjkwMTUzNjg2NDk5MzY4.Xw0Srg.Y6xwY481QrIGqcbHpv2fF4a5ZxY";
 		try {
 			JDA jda = JDABuilder.createDefault(token).build();
+			
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
