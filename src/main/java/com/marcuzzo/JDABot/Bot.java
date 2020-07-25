e63ef4441ecdf31b773099836280023a2382850b
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-24 21:11:30 -0500

    f

--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -1,23 +1,78 @@
 package com.marcuzzo.JDABot;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.github.twitch4j.TwitchClient;
+import com.github.twitch4j.common.util.TwitchUtils;
+import com.github.twitch4j.helix.TwitchHelix;
+
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Bot {
 
-	public static void main (String[] args) {
+	/*
+	private static String clientID = "gywb3p2x3s9x3rc6eqze6ws2swm2i0";
+	private static String oauth = "glv77l9rkdbi95h02djbglosp21xx9";
+	
+	
+	public final static String getJSON(String channelName) {
+		String line;
+		StringBuilder total = new StringBuilder();
+		URL url;
+		HttpURLConnection connection;
 
-		int port = Integer.parseInt(System.getenv("PORT"));
-	//	String host = "0.0.0.0";
-		
-		String token = "NzMyMjkwMTUzNjg2NDk5MzY4.XxinCA.00eQw8ZSdMq1chDG1v_KpQKTPKU";
 		try {
-			JDA jda = JDABuilder.createDefault(token).build();
+			url = new URL(("https://api.twitch.tv/helix/streams?user_login=" + channelName)); //"https://id.twitch.tv/oauth2/authorize" + 
+										//"?client_id=" + clientID +
+										//"&redirect_uri=https://twitchapps.com/tokengen/" + 
+										//"&response_type=code")); 
 			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
+			connection = (HttpURLConnection)url.openConnection();
+			connection.setRequestMethod("GET");
+			connection.setRequestProperty("Client-ID", clientID);
+			connection.setRequestProperty("Authorization", oauth);
+			
+			InputStream is = connection.getInputStream();
+			BufferedReader in = new BufferedReader(new InputStreamReader(is));
+			while ((line = in.readLine()) != null) {
+				total.append(line);
+			}
+			in.close();
+			
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
+		  catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return total.toString();
+	}
+	*/
+	
+	
+	
+	public static void main (String[] args) {
+
+		String token = "NzMyMjkwMTUzNjg2NDk5MzY4.XxinCA.00eQw8ZSdMq1chDG1v_KpQKTPKU";	
+		try {
+			JDA jda = JDABuilder.createDefault(token).build();
+			MessageListener mes = new MessageListener();
+			jda.addEventListener(mes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		//getJSON("Clonetrooper_332");
+		
+		//oauth: glv77l9rkdbi95h02djbglosp21xx9
 		
 	}
 }
---
+++ src/main/java/com/marcuzzo/JDABot/MessageListener.java
@@ -0,0 +1,45 @@
+package com.marcuzzo.JDABot;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MessageListener extends ListenerAdapter
+{
+    public static void main(String[] args)
+          
+    {
+        JDA jda = null;
+		try {
+			jda = JDABuilder.createDefault("token").build();
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        //You can also add event listeners to the already built JDA instance
+        // Note that some events may not be received if the listener is added after calling build()
+        // This includes events such as the ReadyEvent
+        jda.addEventListener(new MessageListener());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().equals("!test"))
+        {
+            MessageChannel channel = event.getChannel();
+            long time = System.currentTimeMillis();
+            channel.sendMessage("Pong!") /* => RestAction<Message> */
+                   .queue(response /* => Message */ -> {
+                       response.editMessageFormat("Tested %d ms", System.currentTimeMillis() - time).queue();
+                   });
+        }
+    }
+}
