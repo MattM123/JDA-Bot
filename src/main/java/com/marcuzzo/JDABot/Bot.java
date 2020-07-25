37e46f456ff964be0e6b6ee0a8656ef33bdebc30
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-25 01:37:59 -0500

    g

--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -2,7 +2,7 @@ package com.marcuzzo.JDABot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import commands.MessageListener;
+import commands.MapCommand;
 
 public class Bot {	
 	
@@ -11,7 +11,7 @@ public class Bot {
 		String token = "NzMyMjkwMTUzNjg2NDk5MzY4.XxinCA.00eQw8ZSdMq1chDG1v_KpQKTPKU";	
 		try {
 			JDA jda = JDABuilder.createDefault(token).build();
-			jda.addEventListener(new MessageListener());
+			jda.addEventListener(new MapCommand());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
