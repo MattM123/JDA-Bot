d93eb572b039f26b498de0ed5ae30bdc1b11aa44
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-25 14:50:28 -0500

    f

--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -2,9 +2,19 @@ package com.marcuzzo.JDABot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.internal.entities.GuildImpl;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import commands.MapCommand;
 
 public class Bot {	
+	
+	
 	
 	public static void main (String[] args) {
 
@@ -12,13 +22,10 @@ public class Bot {
 		try {
 			JDA jda = JDABuilder.createDefault(token).build();
 			jda.addEventListener(new MapCommand());
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		//getJSON("Clonetrooper_332");
-		
-		//oauth: glv77l9rkdbi95h02djbglosp21xx9
 		
 	}
 }
