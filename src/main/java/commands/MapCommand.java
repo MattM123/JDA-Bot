6c9400d8c940bc52d3b03665218e7371ba8c238b
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-25 01:45:04 -0500

    -F

---
+++ src/main/java/commands/MapCommand.java
@@ -0,0 +1,25 @@
+package commands;
+import java.awt.Color;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class MapCommand extends ListenerAdapter {
+	
+	@Override
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+		super.onGuildMessageReceived(event);
+		
+		EmbedBuilder embed = new EmbedBuilder();
+		embed.setTitle("BTE Midwest Map");
+		embed.setColor(Color.magenta);
+		embed.setImage("https://imgur.com/gallery/dTQ4r5e");
+		
+		if (event.getMessage().getContentRaw().equalsIgnoreCase("!map")){
+			event.getChannel().sendMessage(embed.build()).queue();
+		}
+		
+	}
+}
+
