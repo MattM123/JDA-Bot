7d5de67f0f9ae22518dc9366a3bd7a5b3066e464
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-24 23:53:38 -0500

    f

--- src/main/java/commands/MessageListener.java
+++ src/main/java/commands/MessageListener.java
@@ -1,20 +1,11 @@
 package commands;
-import javax.security.auth.login.LoginException;
-import com.apollographql.apollo.api.Response;
-
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.ChannelType;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MessageListener extends ListenerAdapter {
 	
 	@Override
-	public void onGuildMessageRecieved(GuildMessageReceivedEvent event) {
+	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		super.onGuildMessageReceived(event);
 		
 		
