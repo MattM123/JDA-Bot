7ab700ab3429eb5650293bc3a082a4abd6f5b0ed
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-26 16:08:00 -0500

    g

--- src/main/java/commands/ServerStatusCommand.java
+++ src/main/java/commands/ServerStatusCommand.java
@@ -1,23 +1,16 @@
 package commands;
 
-import java.util.List;
-
-import com.stanjg.ptero4j.PteroAdminAPI;
-import com.stanjg.ptero4j.controllers.admin.ServersController;
-import com.stanjg.ptero4j.controllers.admin.UsersController;
-import com.stanjg.ptero4j.controllers.user.UserServersController;
-import com.stanjg.ptero4j.entities.panel.admin.Server;
+import com.stanjg.ptero4j.PteroUserAPI;
 import com.stanjg.ptero4j.entities.panel.user.UserServer;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class ServerStatusCommand extends ListenerAdapter {
 	
 
-	private static  PteroAdminAPI api = new PteroAdminAPI("https://witherpanel.com/", "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens");
+	private static  PteroUserAPI api = new PteroUserAPI("https://witherpanel.com/", "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens");
 	private static String name;
 
 
@@ -26,9 +19,11 @@ public class ServerStatusCommand extends ListenerAdapter {
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		super.onGuildMessageReceived(event);
 
-		Server server = (Server) api.getServersController().getServers("ef773a66");
-		name = server.getName();
-
+		UserServer server = api.getServersController().getServer("ef773a66");
+		
+		//UserServersController controller = api.getServersController();
+		//UserServer server = controller.getServer("aaaa88");
+		name = server.getId();
 		
 		EmbedBuilder embed = new EmbedBuilder();
 		embed.setTitle("Nebraska/Iowa Build Server Status");
@@ -41,9 +36,7 @@ public class ServerStatusCommand extends ListenerAdapter {
 		
 	}
 	
-	 public static void main(String[] args) {
-		 System.out.println(name);
-	 }
+
 
 }
 
