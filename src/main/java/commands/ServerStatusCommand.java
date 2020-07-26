49eef44daddb6b030fe569e55c9d4fd8a19a46dd
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-26 13:54:57 -0500

    g

--- src/main/java/commands/ServerStatusCommand.java
+++ src/main/java/commands/ServerStatusCommand.java
@@ -1,46 +1,48 @@
 package commands;
 
+import java.util.List;
+
 import com.stanjg.ptero4j.PteroAdminAPI;
 import com.stanjg.ptero4j.controllers.admin.ServersController;
+import com.stanjg.ptero4j.controllers.admin.UsersController;
+import com.stanjg.ptero4j.controllers.user.UserServersController;
 import com.stanjg.ptero4j.entities.panel.admin.Server;
+import com.stanjg.ptero4j.entities.panel.user.UserServer;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class ServerStatusCommand extends ListenerAdapter {
-	private static  PteroAdminAPI api = new PteroAdminAPI("https://witherpanel.com/", "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens");
 	
+
+	private static  PteroAdminAPI api = new PteroAdminAPI("https://witherpanel.com/", "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens");
+
+
 	
 	@Override
-	
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		super.onGuildMessageReceived(event);
-		 
 
-		 
-	//	 if (statusString == "false") {
-		//	 embed.addField("Status: ", "Offline", true);
-	//	 }
-	//	 
-	//	 else if (statusString == "true") {
-	//		 embed.addField("Status: ", "Online", true);
-	//	 }
-		Server server = api.getServersController().getServer(12 /* Server ID */); 
+		Server server = (Server) api.getServersController().getServers("ef773a66");
 		String name = server.getName();
+
 		
 		EmbedBuilder embed = new EmbedBuilder();
-		
+		embed.setTitle("Nebraska/Iowa Server Status");
 		embed.addField("Server Name: " , name, true);
-		if (event.getMessage().getContentRaw().equals("!server status")) {
+		
+		if (event.getMessage().getContentRaw().equals("!server")) {
 			
 			event.getChannel().sendMessage(embed.build());
 		}
 		
 	}
 	
-//	public static void main (String[]args) {
-//		
-//	}
+	 public static void main(String[] args) {
+		 
+	 }
+
 }
 
