8d671a1d429d999752af6c6a739faf42f1027553
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-26 16:29:24 -0500

    f

--- src/main/java/commands/ServerStatusCommand.java
+++ src/main/java/commands/ServerStatusCommand.java
@@ -8,16 +8,14 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class ServerStatusCommand extends ListenerAdapter {
-	
-	private static PteroUserAPI api = new PteroUserAPI("https://witherpanel.com/", "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens");
-	private static UserServer server = api.getServersController().getServer("ef773a66");
-	private static String name = server.getName();;
 
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		super.onGuildMessageReceived(event);
 
-
+		PteroUserAPI api = new PteroUserAPI("https://witherpanel.com/", "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens");
+		UserServer server = api.getServersController().getServer("ef773a66");
+		String name = server.getName();
 		
 		
 		//UserServersController controller = api.getServersController();
@@ -31,14 +29,11 @@ public class ServerStatusCommand extends ListenerAdapter {
 		
 		
 		if (event.getMessage().getContentRaw().equalsIgnoreCase("!server")) {
-				event.getChannel().sendMessage(embed.build());
+				event.getChannel().sendMessage(embed.build()).queue();;
 			
 		}
 		
 	}
 	
-	public static void main (String[]args) {
-		System.out.println(name);
-	}
 }
 
