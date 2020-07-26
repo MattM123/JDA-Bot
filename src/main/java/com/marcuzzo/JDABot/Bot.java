678391e13055b6a0102f8b2f23a6555b1edb9a5a
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-26 18:23:42 -0500

    f

--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -3,7 +3,6 @@ package com.marcuzzo.JDABot;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import commands.MapCommand;
-import commands.ServerStateCommand;
 import commands.ServerStatusCommand;
 
 public class Bot {	
