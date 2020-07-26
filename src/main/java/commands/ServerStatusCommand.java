43684eb588cdd247ec66fc0c5082435f5dcd6aaf
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-26 17:25:57 -0500

    f

--- src/main/java/commands/ServerStatusCommand.java
+++ src/main/java/commands/ServerStatusCommand.java
@@ -1,6 +1,10 @@
 package commands;
 
+import java.awt.Color;
+
 import com.stanjg.ptero4j.PteroUserAPI;
+import com.stanjg.ptero4j.entities.objects.server.PowerState;
+import com.stanjg.ptero4j.entities.objects.server.ServerUsage;
 import com.stanjg.ptero4j.entities.panel.user.UserServer;
 
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -17,26 +21,81 @@ public class ServerStatusCommand extends ListenerAdapter {
 		return name;
 	}
 	
+	public static String serverStatus() {
+		PowerState b = server.getPowerState();
+		String status = "";
+		
+		if (b.toString() == "ON") {
+			status = "ONLINE";
+		}
+		
+		else if (b.toString() == "OFF") {
+			status = "OFFLINE";
+		}
+		return status;
+	}
+	
+	
+	public static String cpuUsage() {
+		ServerUsage s = server.getServerUsage();
+		return s.getCpuUsage() + "/" + server.getLimits().getCpu() + "%";
+	}
+	
+	public static String diskUsage() {
+		ServerUsage s = server.getServerUsage();
+		return s.getDiskUsage() + "/" + server.getLimits().getDisk() + "MB";
+	}
+	
+	public static String memoryUsage() {
+		ServerUsage s = server.getServerUsage();
+		return s.getMemoryUsage() + "/" + server.getLimits().getMemory() + "MB";
+	
+	}
+
+	
+	
+	
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		super.onGuildMessageReceived(event);
+
 		
 		EmbedBuilder embed = new EmbedBuilder();
-		embed.setTitle("Nebraska/Iowa Build Server Status");
-		embed.addField("Test", "Test", false);
+		embed.setTitle("Nebraska/Iowa Build Server Status").setColor(Color.blue);
 		embed.addField("Server Name: " , serverName(), true);
+		embed.addField("Server Status: ", serverStatus(), true);
+		embed.addField("CPU Usage: ", cpuUsage(), true);
+		embed.addField("Disk Usage: ", diskUsage(), true);
+		embed.addField("Memory Ussage: ", memoryUsage(), true);
 		
 		
-		if (event.getMessage().getContentRaw().equalsIgnoreCase("!server")) {
+		if (event.getMessage().getContentRaw().equalsIgnoreCase("!server status")) {
 			event.getChannel().sendMessage(embed.build()).queue();
 			
 		}
 		
+		else if (event.getMessage().getContentRaw().equalsIgnoreCase("!server restart") && event.getMessage().getAuthor().getId() == "387330197420113930" ) {
+			server.restart();
+			event.getChannel().sendMessage("Server Restarting...").queue();
+		}
+		
+		else if (event.getMessage().getContentRaw().equalsIgnoreCase("!server start") && event.getMessage().getAuthor().getId() == "387330197420113930") {
+			if (serverStatus() == "ONLINE") {
+				event.getChannel().sendMessage("Server already running!").queue();
+			}
+			else {
+				server.start();
+				event.getChannel().sendMessage("Server Starting...").queue();
+			}
+		}
+		else if (event.getMessage().getContentRaw().equalsIgnoreCase("!server stop")) {
+			server.stop();
+			event.getChannel().sendMessage("Server Stopping...").queue();
+		}
+		
 	}
-	
 	public static void main (String[]args) {
-		System.out.println(serverName());
+		System.out.println(memoryUsage());
 	}
-	
 }
 
