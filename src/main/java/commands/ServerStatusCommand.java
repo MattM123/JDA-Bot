d563a3636d425cfa911e23f37d396e097f42633a
MattM123 <42529379+MattM123@users.noreply.github.com>
2020-07-26 16:38:42 -0500

    f

--- src/main/java/commands/ServerStatusCommand.java
+++ src/main/java/commands/ServerStatusCommand.java
@@ -9,30 +9,34 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class ServerStatusCommand extends ListenerAdapter {
 
+	private static PteroUserAPI api = new PteroUserAPI("https://witherpanel.com/", "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens");
+	private static UserServer server = api.getServersController().getServer("ef773a66");
+	
+	public static String serverName() {
+		String name = server.getName();
+		return name;
+	}
+	
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		super.onGuildMessageReceived(event);
-
-		PteroUserAPI api = new PteroUserAPI("https://witherpanel.com/", "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens");
-		UserServer server = api.getServersController().getServer("ef773a66");
-		String name = server.getName();
-		
-		
-		//UserServersController controller = api.getServersController();
-		//UserServer server = controller.getServer("aaaa88");
-	 
 		
 		EmbedBuilder embed = new EmbedBuilder();
 		embed.setTitle("Nebraska/Iowa Build Server Status");
 		embed.addField("Test", "Test", false);
-		embed.addField("Server Name: " , name, true);
+		embed.addField("Server Name: " , serverName(), true);
 		
 		
 		if (event.getMessage().getContentRaw().equalsIgnoreCase("!server")) {
-				event.getChannel().sendMessage(embed.build()).queue();
+			event.getChannel().sendMessage(serverName()).queue();
+			event.getChannel().sendMessage(embed.build()).queue();
 			
 		}
 		
+	}
+	
+	public static void main (String[]args) {
+		System.out.println(serverName());
 	}
 	
 }
