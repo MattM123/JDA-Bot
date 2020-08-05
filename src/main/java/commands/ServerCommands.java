64648cc5b61275cf9b2a76d05081cb8b816df86e
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-08-05 12:08:49 -0500

    g

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -10,7 +10,6 @@ import com.stanjg.ptero4j.entities.objects.server.ServerUsage;
 import com.stanjg.ptero4j.entities.panel.user.UserServer;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
