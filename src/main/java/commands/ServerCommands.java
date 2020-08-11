e80ea5e3900dc58ab8f2408c1bc20505b6f0e88b
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-08-10 20:51:40 -0500

    g

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -90,7 +90,7 @@ public class ServerCommands extends ListenerAdapter {
 	
 		//password generator
 		if (event.getMessage().getContentRaw().equalsIgnoreCase("!passwordgen")) {
-			event.getChannel().sendMessage("Stored = " + this.stored);
+			event.getChannel().sendMessage("Stored = " + this.stored).queue();
 			if (this.stored.isEmpty() == true || idlong == event.getMessage().getAuthor().getIdLong()) {
 				this.stored.replace(".*", generatedString);
 				User user = event.getMessage().getAuthor();
