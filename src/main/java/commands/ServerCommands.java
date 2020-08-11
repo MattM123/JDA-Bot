02a1cd973e6b620a932c12da539cba074a8e237f
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-08-10 22:30:18 -0500

    g

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -17,7 +17,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class ServerCommands extends ListenerAdapter {
 
 	private static String apikey = "NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens";
-	private static String serverID = "";
+	private static String serverID = "ef773a66";
 	private static PteroUserAPI api = new PteroUserAPI("https://witherpanel.com/", apikey);
 	//NXRD3enHrACazTV2sXDERw7e2pPJYNPmK1YzVYJJ4XzdWens
 	private static UserServer server = api.getServersController().getServer(serverID);
