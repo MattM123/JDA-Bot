c4e177783de059f1d47f0dea4ecc30ad09b50419
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-08-10 22:51:36 -0500

    g\

--- src/main/java/commands/HelpCommand.java
+++ src/main/java/commands/HelpCommand.java
@@ -16,9 +16,9 @@ public class HelpCommand extends ListenerAdapter {
 		embed.setTitle("Command List").setColor(Color.blue);
 		embed.addField("!passwordgen" , "Generates an initial password for first time use with the bot.", false);
 		embed.addField("!setpassword <currentpassword>", "Requires permissions to regenerate the password.", false);
-		embed.addField("!server start", "SRequires permissions to start the server.", false);
+		embed.addField("!server start", "Requires permissions to start the server.", false);
 		embed.addField("!server stop", "Requires permissions to stop the server.", false);
-		embed.addField("!Server restart", "Requires permissions to restart the server.", false);
+		embed.addField("!server restart", "Requires permissions to restart the server.", false);
 		embed.addField("!console <command>", "Requires permissions to send server console commands", false);
 		embed.addField("!server status", "Shows the server status", false);
 		embed.addField("!map", "Shows the current map of Midwest states that are being built", false);
