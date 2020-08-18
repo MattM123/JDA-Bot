2f28eae57eee760909df17310b4d83fb410dd2f5
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-08-18 12:30:51 -0500

    F

--- src/main/java/commands/HelpCommands.java
+++ src/main/java/commands/HelpCommands.java
@@ -1,16 +1,7 @@
 package commands;
 
 import java.awt.Color;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.EmbedType;
-import net.dv8tion.jda.api.entities.MessageEmbed;
-import net.dv8tion.jda.api.entities.MessageEmbed.Provider;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
