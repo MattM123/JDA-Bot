6a58f823e7c831684895f2fe28ccc5fb5345941f
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-08-18 01:39:06 -0500

    f

--- src/main/java/commands/HelpCommands.java
+++ src/main/java/commands/HelpCommands.java
@@ -1,7 +1,10 @@
 package commands;
 
 import java.awt.Color;
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
@@ -25,11 +28,19 @@ public class HelpCommands extends ListenerAdapter {
 		}
 		
 		if (event.getMessage().getContentRaw().equalsIgnoreCase("!measure")) {
+
 			EmbedBuilder measure = new EmbedBuilder();
-			File file = new File("measurecommand.mp4");
-			measure.setImage("attachment://measurecommand.mp4");
-			
-			event.getChannel().sendFile(file).embed(measure.build()).queue();
+			InputStream file = null;
+			try {
+				file = new URL("https://i.gyazo.com/d58446cec35cc504bb36b749346041a9.mp4").openStream();
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			measure.setImage("attachment://measurecommand.mp4"); // we specify this in sendFile as "cat.png"
+			    
+			event.getChannel().sendFile(file, "measurecommand.mp4").embed(measure.build()).queue();
 
 		}		
 	}
