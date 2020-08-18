e5e27da2ef0681572daa31f33b587f5ceb56f6c8
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-08-18 02:11:31 -0500

    f

--- src/main/java/commands/HelpCommands.java
+++ src/main/java/commands/HelpCommands.java
@@ -7,6 +7,9 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.EmbedType;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.MessageEmbed.Provider;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -29,20 +32,11 @@ public class HelpCommands extends ListenerAdapter {
 		
 		if (event.getMessage().getContentRaw().equalsIgnoreCase("!measure")) {
 		
-			EmbedBuilder measure = new EmbedBuilder();
-			measure.setColor(Color.blue);
-			
-			//InputStream file = null;
-		//	try {
-		//		file = new URL("https://gyazo.com/d58446cec35cc504bb36b749346041a9").openStream();
-		//	} catch (MalformedURLException e) {
-		//		e.printStackTrace();
-		////	} catch (IOException e) {
-		//		e.printStackTrace();
-		//	}
-			measure.setImage("https://gyazo.com/d58446cec35cc504bb36b749346041a9");
+			MessageEmbed measure = new MessageEmbed("https://gyazo.com/d58446cec35cc504bb36b749346041a9",
+					"Measuring Tutorial", null, EmbedType.VIDEO, null, 10, null,
+					new MessageEmbed.Provider(null, "www.gyazo.com"), null, null, null, null,null);
 			    
-			event.getChannel().sendMessage(measure.build()).queue();
+			event.getChannel().sendMessage(measure).queue();
 
 			 
 			
