e260dfbf177f2208cbd9c63af028ccb07eeb1ddd
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-11-04 22:41:26 -0600

    f

--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -2,6 +2,7 @@ package com.marcuzzo.JDABot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import commands.HelpCommands;
 import commands.MapCommand;
 import commands.ServerCommands;
@@ -14,6 +15,7 @@ public class Bot {
 		try {
 			JDA jda = JDABuilder.createDefault(token).build();
 			
+			jda.getPresence().setActivity(Activity.playing("https://i.imgur.com/0B6Isbo.png"));
 			jda.addEventListener(new MapCommand());
 			jda.addEventListener(new ServerCommands());
 			jda.addEventListener(new HelpCommands());
