538c0d096e295dd6b7dd3bbcb1ebde5d70247d3c
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-12-06 12:44:07 -0600

    f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -336,7 +336,7 @@ public class ServerCommands extends ListenerAdapter {
 					}
 					in.close();
 					
-					event.getChannel().sendMessage("Json: " + json.subSequence(0, 500));
+					event.getChannel().sendMessage("Json: " + json.subSequence(0, 500)).queue();
 					event.getChannel().sendMessage(String.valueOf(conn.getResponseCode())).queue();
 
 				} catch (MalformedURLException e) {
