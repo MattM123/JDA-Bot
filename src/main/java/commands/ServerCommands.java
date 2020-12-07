cc0194aa9802fa33ef2253997132df92c256dc6e
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2020-12-06 21:02:41 -0600

    f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -342,7 +342,7 @@ public class ServerCommands extends ListenerAdapter {
 				//	JSONArray jarray = jobj.getJSONArray("members");
 					
 					
-					event.getChannel().sendMessage(jobj.getString("discordId").substring(0, 500)).queue();
+					event.getChannel().sendMessage(jobj.get("members").toString().substring(0, 500)).queue();
 					
 					
 				} catch (MalformedURLException e) {
