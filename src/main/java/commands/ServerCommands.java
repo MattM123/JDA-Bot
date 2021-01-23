bece00039d2d04c8ade6d347782adfa343622b45
Matt Marcuzzo <mattmarcuzzo@gmail.com>
2021-01-23 15:39:32 -0600

    f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -357,7 +357,7 @@ public class ServerCommands extends ListenerAdapter {
 				event.getChannel().sendMessage(json.toString()).queue();
 				
 				JsonElement ele = JsonParser.parseString(json.toString());
-				jarray = ele.getAsJsonObject().getAsJsonArray("applications").getAsJsonObject().getAsJsonArray("asnwers");
+				jarray = ele.getAsJsonObject().getAsJsonArray("asnwers");
 				
 				
 			} catch (MalformedURLException e) {
