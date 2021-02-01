6c89dac127218ba04168c4fdeeaaf13a03488f13
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-02-01 15:56:51 -0600

    f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -371,16 +371,16 @@ public class ServerCommands extends ListenerAdapter {
 		
 		//reads server console and sends server message when corrupted areas have been encountered
 		
-		if (event.getChannel().equals(event.getGuild().getGuildChannelById(802232830129995847L))) {
-			if (event.getMessage().getContentRaw().contains("Cube is corrupted!") && (event.getMessage().getContentRaw().contains("Cube will be regenerated")
-				&& (event.getMessage().getContentRaw().contains("ERROR")))); {
-				server.sendCommand("say A corrupted area has been loaded in but it is being regenerated.");
-			}
-			if (event.getMessage().getContentRaw().contains("Could not load cube in") && (event.getMessage().getContentRaw().contains("ERROR"))) {
-				server.sendCommand("say A corrupted area has failed to regenerate.");
-			}
+		//if (event.getChannel().equals(event.getGuild().getGuildChannelById(802232830129995847L))) {
+	//		if (event.getMessage().getContentRaw().contains("Cube is corrupted!") && (event.getMessage().getContentRaw().contains("Cube will be regenerated")
+	//			&& (event.getMessage().getContentRaw().contains("ERROR")))); {
+	//			server.sendCommand("say A corrupted area has been loaded in but it is being regenerated.");
+		//	}
+		//	if (event.getMessage().getContentRaw().contains("Could not load cube in") && (event.getMessage().getContentRaw().contains("ERROR"))) {
+		//		server.sendCommand("say A corrupted area has failed to regenerate.");
+		//	}
 			
-		}
+		//}
 		
 	}	
 	
