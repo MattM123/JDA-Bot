1ab1622340898a6c5ceb0c6b52116446d4a64597
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-02-27 00:44:49 -0600

    -f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -527,11 +527,11 @@ public class ServerCommands extends ListenerAdapter {
 					User testUser = event.getJDA().getUserById(UserLongString);
 					if (RandomApp.toString().equals("{\"applications\":[]}")) {
 						
-						event.getChannel().sendMessage("Inside if").queue();
 						EmbedBuilder testCommand = new EmbedBuilder();
 						testCommand.setTitle("Random user ID selected: " + testUser.getName());
-						testCommand.addField("Predicted Output", "Player merged into team. No application found" + usernameAppliedWith, false);
+						testCommand.addField("Predicted Output", "Player merged into team. No application found." + usernameAppliedWith, false);
 						
+						event.getChannel().sendMessage("After Embed").queue();
 						event.getChannel().sendMessage(testCommand.build()).queue();
 					}
 					
