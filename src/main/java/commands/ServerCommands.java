18bf9addec8e6492abf1a142a05e89d114219f78
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-02-27 00:10:35 -0600

    -f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -516,6 +516,7 @@ public class ServerCommands extends ListenerAdapter {
 					in.close();
 						
 					event.getChannel().sendMessage(" " + ApplicationFromID).queue();
+					event.getChannel().sendMessage(json.toString());
 					//JSON Deserialization
 					
 					Gson gson1 = new Gson();
