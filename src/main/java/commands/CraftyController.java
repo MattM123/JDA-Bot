498eb4ee4423c225eca98a93e24d7e459182228f
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-03-01 19:33:33 -0600

    f

--- src/main/java/commands/CraftyController.java
+++ src/main/java/commands/CraftyController.java
@@ -67,7 +67,7 @@ public class CraftyController {
 
 		try {
 			fixUntrustCertificate();
-			url = new URL("https://97.83.168.213:443/api/v1/host_stats," + apikey);
+			url = new URL("https://panel.richterent.com/api/v1/host_stats," + apikey);
 			conn = (HttpsURLConnection) url.openConnection();
 			//conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
 			conn.setRequestMethod("GET");
