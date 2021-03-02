8238cf46627bca0b48f1f19aeba7bc85d3dba58a
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-03-01 19:35:49 -0600

    f

--- src/main/java/commands/CraftyController.java
+++ src/main/java/commands/CraftyController.java
@@ -71,6 +71,7 @@ public class CraftyController {
 			conn = (HttpsURLConnection) url.openConnection();
 			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36");
 			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setRequestProperty("Accept", "application/json");
 			conn.setRequestMethod("GET");
 			
 			//Storing JSON from request into string. Prints error code and error stream if encountered.
