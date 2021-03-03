d8a73cd42566f8f1c5836df92cb6288ca1e54962
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-03-02 20:47:22 -0600

    f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -142,9 +142,9 @@ public class ServerCommands extends ListenerAdapter {
 			stats.setTitle("Build Server Status");
 			stats.setColor(Color.BLUE);
 			
-			stats.addField("Server Status for NE, IA, MN, KS, MO, IL, OK     ", status1, true);
+			stats.addField("Server Status for NE, IA, MN, KS, MO, IL, OK     ** **", status1, true);
 			stats.addField("Server Status for WI", status, true);
-			
+			stats.addBlankField(true);
 			
 			stats.addField("CPU Usage", ele1.getAsJsonObject().get("cpu_usage") + "%MW", true);
 			stats.addField("CPU Usage", ele.getAsJsonObject().get("cpu_usage") + "%WI", true);
