ef0bfa712ef250d7208fc914312fd1b84595d562
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-03-08 20:05:28 -0600

    f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -278,7 +278,7 @@ public class ServerCommands extends ListenerAdapter {
 			for (int i = 9; i <= charArr.length; i++) {
 				if (i == 9) {
 					event.getChannel().sendMessage("user: " + message.substring(9, message.lastIndexOf("-"))).queue();
-					user += message.substring(9, message.lastIndexOf(" -"));	
+					user += message.substring(9, message.lastIndexOf("-"));	
 					event.getChannel().sendMessage("user: " + user).queue();	
 				}
 				
