d74f65d5f7c054ef614048dc57c9ac64fd0935cc
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-03-09 20:28:31 -0600

    f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -50,7 +50,10 @@ public class ServerCommands extends ListenerAdapter {
 				event.getChannel().sendMessage("Rank updated to Event Builder for user " + namebuilder).queue();
 			}
 			else {
-				event.getChannel().sendMessage("Invalid permissions.").queue();
+				EmbedBuilder emb = new EmbedBuilder();
+				emb.setColor(Color.BLUE);
+				emb.setTitle("Invalid Permissions");
+				event.getChannel().sendMessage(emb.build()).queue();
 			}
 		}
 		
@@ -69,7 +72,10 @@ public class ServerCommands extends ListenerAdapter {
 				event.getChannel().sendMessage("Rank updated to Application In Progress for user " + namebuilder).queue();
 			}
 			else {
-				event.getChannel().sendMessage("Invalid permissions.").queue();
+				EmbedBuilder emb = new EmbedBuilder();
+				emb.setColor(Color.BLUE);
+				emb.setTitle("Invalid Permissions");
+				event.getChannel().sendMessage(emb.build()).queue();
 			}
 		}
 		
@@ -88,7 +94,13 @@ public class ServerCommands extends ListenerAdapter {
 			if (crafty.getServerStats().contains("MalformedURLException") || crafty.getServerStats().contains("IOException") || crafty.getServerStats().contains("JSONException")
 					|| crafty.getServerStats().contains("NoSuchAlgorithmException") || crafty.getServerStats().contains("KeyManagementException")) {
 				
-				event.getChannel().sendMessage(crafty.getServerStats().subSequence(0, 1000)).queue();
+				EmbedBuilder emb = new EmbedBuilder();
+				emb.setColor(Color.BLUE);
+				emb.setTitle("There was an error retrieveing the server stats");
+				emb.addField("", (String) crafty.getServerStats().subSequence(0, 1000), false);
+				event.getChannel().sendMessage(emb.build()).queue();
+				
+		
 			}
 			else {
 				
@@ -179,8 +191,11 @@ public class ServerCommands extends ListenerAdapter {
 				if ((usernameApplied.contains("Error Code: ") || usernameApplied.contains("MalformedURLException") 
 						|| usernameApplied.contains("IOException") || usernameApplied.contains("JSONException")) && usernameApplied.length() > 30) {
 					
-					event.getChannel().sendMessage("There was an error with retrieveing the users' application data.").queue();
-					event.getChannel().sendMessage(usernameApplied).queue();
+					EmbedBuilder emb = new EmbedBuilder();
+					emb.setColor(Color.BLUE);
+					emb.setTitle("There was an error with retrieveing the users' application data.");
+					emb.addField("", usernameApplied, false);
+					event.getChannel().sendMessage(emb.build()).queue();
 				}
 				
 					List<Role> roles = event.getMember().getRoles();
@@ -198,19 +213,32 @@ public class ServerCommands extends ListenerAdapter {
 						
 						for (int i = 0; i < BTE.getMemberList().size(); i++) {	
 							if (roles.contains(guild.getRoleById(735991952931160104L)) && (MCusername.equalsIgnoreCase(usernameApplied))) {
-								event.getChannel().sendMessage("You already have builder role! Assigning server rank.").queue();
+								
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("You already have builder role! Assigning server rank.");
+								event.getChannel().sendMessage(emb.build()).queue();
+								
 								temp = 1;
 								break;
 							}
 										
 							else if (event.getAuthor().getIdLong() == BTE.getMemberList().get(i) && !roles.contains(guild.getRoleById(Long.parseLong("735991952931160104"))) && (MCusername.equalsIgnoreCase(usernameApplied))) {
 								guild.addRoleToMember(event.getMember(), guild.getRoleById(735991952931160104L)).queue();
-								event.getChannel().sendMessage("You now have Builder role!").queue();
+				
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("You now have Builder role!");
+								event.getChannel().sendMessage(emb.build()).queue();
+								
 								temp = 1;
 								break;
 							}
 							else if (!(MCusername.equalsIgnoreCase(usernameApplied))) {
-								event.getChannel().sendMessage("The username you applied with and the one you used to run this command do not match.").queue();
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("The username you applied with and the one you used to run this command do not match.");
+								event.getChannel().sendMessage(emb.build()).queue();
 								break;
 							}
 						}
@@ -220,46 +248,73 @@ public class ServerCommands extends ListenerAdapter {
 						if (temp == 1) {
 							if (roles.contains(guild.getRoleById(735995176165834756L))) {
 								server.sendCommand("lp user " + MCusername + " parent add kansas-builder");
-								event.getChannel().sendMessage("Minecraft server rank updated to Kansas Builder for user " + MCusername).queue();
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("Minecraft server rank updated to Kansas Builder for user" + MCusername);
+								event.getChannel().sendMessage(emb.build()).queue();
 		
 							}
 							else if (roles.contains(guild.getRoleById(735995164493086720L))) {
 								server.sendCommand("lp user " + MCusername + " parent add iowa-builder");
-								event.getChannel().sendMessage("Minecraft server rank updated to Iowa Builder for user " + MCusername).queue();
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("Minecraft server rank updated to Iowa Builder for user" + MCusername);
+								event.getChannel().sendMessage(emb.build()).queue();
 		
 							}
 							else if (roles.contains(guild.getRoleById(735995136978321541L))) {
 								server.sendCommand("lp user " + MCusername + " parent add nebraska-builder");
-								event.getChannel().sendMessage("Minecraft server rank updated to Nebraska Builder for user " + MCusername).queue();
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("Minecraft server rank updated to Nebraska Builder for user" + MCusername);
+								event.getChannel().sendMessage(emb.build()).queue();
 		
 							}
 							else if (roles.contains(guild.getRoleById(735995095773609986L))) {
 								server.sendCommand("lp user " + MCusername + " parent add illinois-builder");
-		
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("Minecraft server rank updated to Illinois Builder for user" + MCusername);
+								event.getChannel().sendMessage(emb.build()).queue();
 							}
 							else if (roles.contains(guild.getRoleById(735995115113414656L))) {
 								server.sendCommand("lp user " + MCusername + " parent add missouri-builder");
-								event.getChannel().sendMessage("Minecraft server rank updated to Missouri Builder for user " + MCusername).queue();
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("Minecraft server rank updated to Missouri Builder for user" + MCusername);
+								event.getChannel().sendMessage(emb.build()).queue();
 		
 							}
 							else if (roles.contains(guild.getRoleById(735995196738633819L))) {
 								server.sendCommand("lp user " + MCusername + " parent add minnesota-builder");
-								event.getChannel().sendMessage("Minecraft server rank updated to Minnesota Builder for user " + MCusername).queue();
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("Minecraft server rank updated to Minnesota Builder for user" + MCusername);
+								event.getChannel().sendMessage(emb.build()).queue();
 							}
 							else if (roles.contains(guild.getRoleById(808415301799641119L))) {
 								server.sendCommand("lp user " + MCusername + " parent add oklahoma-builder");
-								event.getChannel().sendMessage("Minecraft server rank updated to Oklahoma Builder for user " + MCusername).queue();
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("Minecraft server rank updated to Oklahoma Builder for user" + MCusername);
+								event.getChannel().sendMessage(emb.build()).queue();
 							}
 							
 							else {
-								event.getChannel().sendMessage("Looks like you don't have a state role. Go to #role-menu to select one and run the command again to get server build perms.").queue();
+								EmbedBuilder emb = new EmbedBuilder();
+								emb.setColor(Color.BLUE);
+								emb.setTitle("Go to #role-menu to select one and run the command again to get server build perms.");
+								event.getChannel().sendMessage(emb.build()).queue();
 							}
 						}
 						
 						//if user is not on the team at all, print this
 						
 						else if (temp == 0) {
-							event.getChannel().sendMessage("Looks like you're not on the team or your username was invalid. If this is wrong, then ping mattress#1852").queue();
+							EmbedBuilder emb = new EmbedBuilder();
+							emb.setColor(Color.BLUE);
+							emb.setTitle("Looks like you're not on the team or your username was invalid. If this is wrong, then ping mattress#1852");
+							event.getChannel().sendMessage(emb.build()).queue();
 						}
 					}
 		}
