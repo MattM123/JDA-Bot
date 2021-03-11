0f08ebd5ec6b0851bdf296bc56372f79a9967537
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-03-11 14:31:36 -0600

    f

--- src/main/java/commands/CraftyControllerAPI.java
+++ src/main/java/commands/CraftyControllerAPI.java
@@ -138,7 +138,7 @@ public class CraftyControllerAPI {
 			conn.setRequestProperty("Content-Type", "text/html; charset=UTF-8");
 			conn.setRequestProperty("Accept", "text/html");
 			conn.setRequestProperty("Host", "panel.richterent.com");
-			conn.addRequestProperty("body", command);
+			conn.addRequestProperty("command", command);
 			conn.setRequestMethod("POST");
 			
 			
