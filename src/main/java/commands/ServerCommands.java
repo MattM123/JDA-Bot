6477fb37ae2287212ddee0856b042205d47ced72
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-03-11 12:16:26 -0600

    f

--- src/main/java/commands/ServerCommands.java
+++ src/main/java/commands/ServerCommands.java
@@ -177,7 +177,7 @@ public class ServerCommands extends ListenerAdapter {
 				EmbedBuilder emb = new EmbedBuilder();
 				emb.setColor(Color.BLUE);
 				emb.setTitle("An error occured while sending the console command");
-				emb.addField("", crafty.stackTrace, false);
+				emb.addField("Stacktrace", crafty.stackTrace, false);
 				event.getChannel().sendMessage(emb.build()).queue();
 			}
 			else {
