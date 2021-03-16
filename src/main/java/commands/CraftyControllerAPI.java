5851c0a9cd53f19f9ac6ca399721747b92522d0c
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-03-16 13:17:53 -0500

    f

--- src/main/java/commands/CraftyControllerAPI.java
+++ src/main/java/commands/CraftyControllerAPI.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -193,7 +194,9 @@ public class CraftyControllerAPI {
 		    
 		    if (entity != null) {
 		    	InputStream instream = entity.getContent();
-		        stackTrace = IOUtils.toString(instream, "UTF-8"); 
+		    	StringWriter writer = new StringWriter();
+		    	IOUtils.copy(instream, writer, "UTF-8");
+		    	stackTrace = writer.toString(); 
 		    }
 		    
 		    client.close();	
