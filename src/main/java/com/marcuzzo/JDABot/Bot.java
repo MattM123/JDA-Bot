0872ddfb047313159d6c7135e81f214acc4012c7
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-06-13 15:04:10 -0500

    F

--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -12,7 +12,7 @@ public class Bot {
 
 	public static void main (String[] args) {
 		
-		String token = "NzMyMjkwMTUzNjg2NDk5MzY4.XwycWQ.AHOlF_0KI7GeFj4-Oyxu2-_3gpM"; //token used to hook into the Discord bot (bot token)
+		String token = "NzMyMjkwMTUzNjg2NDk5MzY4.XwycWQ.DV6g4DyHxlI01yxROtKMggZFW_Q"; //token used to hook into the Discord bot (bot token)
 		try {
 			JDA jda = JDABuilder.createDefault(token)
 					.setMemberCachePolicy(MemberCachePolicy.ALL)
