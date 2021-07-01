96fc09f20db367270e06c6935a1476c59095d969
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-06-30 22:52:50 -0500

    f

--- src/main/java/commands/CraftyControllerAPI.java
+++ src/main/java/commands/CraftyControllerAPI.java
@@ -34,6 +34,7 @@ public class CraftyControllerAPI {
 		URL url;
 		HttpsURLConnection conn = null;
 		
+		
 
 		try {
 			url = new URL("https://panel.richterent.com/api/v1/server_stats?token=" + apikey);
