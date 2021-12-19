8b5efd7155f2d265ff377ed11654463971df6f5d
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-12-18 20:26:45 -0600

    web scraping test

--- src/main/java/commands/APICommands.java
+++ src/main/java/commands/APICommands.java
@@ -97,7 +97,7 @@ public class APICommands extends ListenerAdapter {
 				e.printStackTrace();
 			}
 			 
-			 event.getChannel().sendMessage(doc.getElementsByTag("body").get(0).getElementById("app-mount").getElementsByClass("app-1q1i1E").get(0).toString()).queue();
+			 event.getChannel().sendMessage(doc.getElementsByTag("body").get(0).getElementById("app-mount").getElementsByTag("div").toString()).queue();
 			 
 			 
 		 }
