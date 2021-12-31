129b9b728d9ac12ae0cc05b287640f2bbed92fae
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-12-30 23:08:46 -0600

    PippenTracker test

--- src/main/java/commands/NonAPICommands.java
+++ src/main/java/commands/NonAPICommands.java
@@ -188,7 +188,7 @@ public class NonAPICommands extends ListenerAdapter {
 
 		if (event.getMessage().getChannel().equals(pippenSubmissionChannel) && event.getMessage().getContentRaw().contains("PippenFTS#3088")) {
 			pippenPoints++;
-			pippenTrackerChannel.sendMessage("PippenTracker 1.0").queue();
+			//pippenTrackerChannel.sendMessage("PippenTracker 1.0").queue();
 			
 			pippenTrackerChannel.retrieveMessageById(pippenTrackerChannel.getLatestMessageIdLong()).queue((message) -> {
 					pippenTrackerChannel.editMessageById(pippenTrackerChannel.getLatestMessageIdLong(),"**__You need more then " + pippenPoints + " completed buildings to beat Pippen!__**").queue();
