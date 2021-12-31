f0bc120a67ddc671f3fde8a7e48a830eb81ce97d
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-12-31 11:41:54 -0600

    buildtracker test

--- src/main/java/commands/NonAPICommands.java
+++ src/main/java/commands/NonAPICommands.java
@@ -233,8 +233,9 @@ public class NonAPICommands extends ListenerAdapter {
 						
 						//Incrementing build count and editing the count
 						for (int i = message.getContentRaw().indexOf(event.getAuthor().getAsTag().substring(0, event.getAuthor().getAsTag().length() - 5) + " : "); i < message.getContentRaw().length(); i++) {
-							if (message.getContentRaw().charAt(i) == ':' && message.getContentRaw().substring(i - (event.getAuthor().getAsTag().length() - 5), 
+							if (message.getContentRaw().charAt(i) == ':' && message.getContentRaw().substring(Math.abs(i - (event.getAuthor().getAsTag().length() - 5)), 
 									event.getAuthor().getAsTag().length() - 5).equals(event.getAuthor().getAsTag().substring(0, event.getAuthor().getAsTag().length() - 5))) {
+								
 									event.getChannel().sendMessage(message.getContentRaw().substring(i - (event.getAuthor().getAsTag().length() - 5), 
 									event.getAuthor().getAsTag().length() - 5)).queue();
 								message.editMessage(message.getContentRaw().replace(message.getContentRaw().substring(i + 2, i + 2 + counter.length()), String.valueOf(Integer.parseInt(counter) + 1))).queue();
