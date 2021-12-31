fd7f7e9d81d3564b44e1d8569ce4cfae3374109c
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-12-31 10:39:05 -0600

    buildtracker test

--- src/main/java/commands/NonAPICommands.java
+++ src/main/java/commands/NonAPICommands.java
@@ -233,7 +233,8 @@ public class NonAPICommands extends ListenerAdapter {
 						//Incrementing build count and editing the count
 						for (int i = message.getContentRaw().indexOf(event.getAuthor().getAsTag().substring(0, event.getAuthor().getAsTag().length() - 5) + " : "); i < message.getContentRaw().length(); i++) {
 							if (message.getContentRaw().charAt(i) == ':') {
-								event.getChannel().sendMessage(String.valueOf(Integer.parseInt(counter)));
+								event.getChannel().sendMessage(String.valueOf(Integer.parseInt(counter))).queue();
+								event.getChannel().sendMessage(String.valueOf(Integer.parseInt(counter) + 1)).queue();
 								message.editMessage(message.getContentRaw().replace(message.getContentRaw().substring(i + 2, i + 2 + counter.length()), String.valueOf(Integer.parseInt(counter) + 1))).queue();
 							}
 						}
