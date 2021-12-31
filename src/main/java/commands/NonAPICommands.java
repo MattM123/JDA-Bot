2f22906bbfb051dbf2904b520b03dec954d3f444
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2021-12-31 11:49:07 -0600

    buildtracker test

--- src/main/java/commands/NonAPICommands.java
+++ src/main/java/commands/NonAPICommands.java
@@ -233,15 +233,16 @@ public class NonAPICommands extends ListenerAdapter {
 						
 						//Incrementing build count and editing the count
 						for (int i = message.getContentRaw().indexOf(event.getAuthor().getAsTag().substring(0, event.getAuthor().getAsTag().length() - 5) + " : "); i < message.getContentRaw().length(); i++) {
-							if (message.getContentRaw().charAt(i) == ':' && message.getContentRaw().substring(Math.abs(i - (event.getAuthor().getAsTag().length() - 5)), 
+							if (message.getContentRaw().charAt(i) == ':') {
+								if (message.getContentRaw().substring(Math.abs(i - (event.getAuthor().getAsTag().length() - 5)), 
 									event.getAuthor().getAsTag().length() - 5).equals(event.getAuthor().getAsTag().substring(0, event.getAuthor().getAsTag().length() - 5))) {
 								
 									event.getChannel().sendMessage(message.getContentRaw().substring(i - (event.getAuthor().getAsTag().length() - 5), 
 									event.getAuthor().getAsTag().length() - 5)).queue();
-								message.editMessage(message.getContentRaw().replace(message.getContentRaw().substring(i + 2, i + 2 + counter.length()), String.valueOf(Integer.parseInt(counter) + 1))).queue();
+									message.editMessage(message.getContentRaw().replace(message.getContentRaw().substring(i + 2, i + 2 + counter.length()), String.valueOf(Integer.parseInt(counter) + 1))).queue();
+								}
 							}
 						}
-		
 					}
 					//if no count exists for user
 					if (!message.getContentRaw().contains(event.getAuthor().getAsTag().substring(0, event.getAuthor().getAsTag().length() - 5) + " : ")) {
