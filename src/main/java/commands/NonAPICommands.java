77489c716fbedf89f6834ba767f19ccb1528cb7c
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-01-03 09:22:20 -0600

    updates

--- src/main/java/commands/NonAPICommands.java
+++ src/main/java/commands/NonAPICommands.java
@@ -217,7 +217,7 @@ public class NonAPICommands extends ListenerAdapter {
 			pippenTrackerChannel.retrieveMessageById(pippenTrackerChannel.getLatestMessageIdLong()).queue((message) -> {
 				for (int i = 24; i < message.getContentRaw().length(); i++) {
 					try {
-						int s = Integer.parseInt("" + i);	
+						int s = Integer.parseInt("" + message.getContentRaw().charAt(i));	
 						pippenPoints += String.valueOf(s);
 	
 					}
