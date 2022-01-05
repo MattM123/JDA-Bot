8eb7c95fea4b21a2024db86d2014044cb30306c5
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-01-05 15:47:49 -0600

    build system test

--- src/main/java/commands/NonAPICommands.java
+++ src/main/java/commands/NonAPICommands.java
@@ -297,9 +297,9 @@ public class NonAPICommands extends ListenerAdapter {
 		
 		//If reaction was used in submission channel
 		if (event.getReaction().getChannel().equals(builderSubmissions)) {	
-			event.getChannel().sendMessage(event.getReactionEmote().getAsReactionCode()).queue();
-			if (event.getReactionEmote().getAsReactionCode().equals(":white_check_mark:")) {
-				event.getChannel().sendMessage("test").queue();
+			event.getChannel().sendMessage(ReactionEmote.fromUnicode("\\u2705", guild.getJDA()).getAsReactionCode()).queue();
+			if (event.getReactionEmote().getAsReactionCode().equals(ReactionEmote.fromUnicode("\\u2705", guild.getJDA()).getAsReactionCode())) {
+				event.getChannel().sendMessage("test").queue(); 
 			}	
 		}
 	}
