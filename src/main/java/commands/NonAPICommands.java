1e87e0301cf3ae25f36f437bad1d3c871315fb17
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-01-05 19:57:42 -0600

    build system test

--- src/main/java/commands/NonAPICommands.java
+++ src/main/java/commands/NonAPICommands.java
@@ -30,7 +30,7 @@ public class NonAPICommands extends ListenerAdapter {
 	
 	private String pippenPoints = "";
 	private String counter = "";
-	private File buildCounts = new File("//app//BuildCountData");
+	private File buildCounts = new File("/JDABot/src/main/java/commands/BuildCountData.txt");
 	
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
