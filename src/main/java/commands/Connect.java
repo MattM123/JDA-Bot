d23e11496b29a2f453ff8554282cf3feb873ea68
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-01-06 20:47:24 -0600

    build system test

--- src/main/java/commands/Connect.java
+++ src/main/java/commands/Connect.java
@@ -11,15 +11,13 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 public class Connect {  
-	private static String fileName = "builderData.db";
 	private static Connection conn = null;
 	private static Guild guild = Bot.jda.getGuildById(735990134583066679L);
 	private static TextChannel errorlog = guild.getTextChannelById(928432209872977990L);
 	
 	public static Connection connect() { 
 		try {  
-			DriverManager.registerDriver(new org.sqlite.JDBC());
-			conn = DriverManager.getConnection(System.getenv("DATABASE_URL"));		
+			conn = DriverManager.getConnection(System.getenv("DATABASE_URL"), "postgres", System.getenv("DATABASE_PASSWORD"));		
              
 		} catch (SQLException e) {  
 			errorlog.sendMessage(e.getMessage()).queue();
