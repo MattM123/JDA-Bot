45c12232afded97f8e04f205bd1d2a5cea4d91b2
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-01-09 13:51:20 -0600

    column fix attempt

--- src/main/java/commands/BuildLeaderboard.java
+++ src/main/java/commands/BuildLeaderboard.java
@@ -56,8 +56,8 @@ public class BuildLeaderboard extends Paginator.Builder {
 			
 			rs.last();
 			String[] addThis = new String[rs.getRow()];
-			String[] names = new String[rs.getRow()];
-			String[] counts = new String[rs.getRow()];
+			String[] names = new String[rs.getRow() - 1];
+			String[] counts = new String[rs.getRow() - 1];
 			int pointer = 0;
 			char[] namespace = "᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼᲼".toCharArray(); //size 35
 			char[] countspace = "᲼᲼᲼᲼᲼".toCharArray(); //size 5
