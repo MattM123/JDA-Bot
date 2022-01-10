94f225f8fd78d13321d25fa52a58ad6faa58ec64
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-01-09 19:33:31 -0600

    column fix attempt

--- src/main/java/commands/BuildLeaderboard.java
+++ src/main/java/commands/BuildLeaderboard.java
@@ -98,7 +98,7 @@ public class BuildLeaderboard extends Paginator.Builder {
 				*/
 				}
 			}
-			this.addItems("test");
+			this.setItems("test", "test");
 			for (int i = 0; i < names.length; i++) {
 				this.addItems(names[i]);
 				this.addItems(counts[i]);
