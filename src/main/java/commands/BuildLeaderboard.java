9b94f8c10fd7624a49500c0148787056db1e19c2
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-01-10 18:13:59 -0600

    leaderboard rework

--- src/main/java/commands/BuildLeaderboard.java
+++ src/main/java/commands/BuildLeaderboard.java
@@ -109,19 +109,29 @@ public class BuildLeaderboard extends EmbedPaginator.Builder {
 				for (int i = 0; i < items.size(); i += 10) {
 					EmbedBuilder emb = new EmbedBuilder();
 					
+					
 					try {
 						emb.setColor(Color.blue);
+						
 						emb.addField(items.get(i), "", true);
 						emb.addField(items.get(i + 1), "", true);
-							
+						
+						emb.addField("" + '\u200b', "" + '\u200b', true);
+
 						emb.addField(items.get(i + 2), "", true);
 						emb.addField(items.get(i + 3), "", true);
+						
+						emb.addField("" + '\u200b', "" + '\u200b', true);
 							
 						emb.addField(items.get(i + 4), "", true);
 						emb.addField(items.get(i + 5), "", true);
+						
+						emb.addField("" + '\u200b', "" + '\u200b', true);
 							
 						emb.addField(items.get(i + 6), "", true);
 						emb.addField(items.get(i + 7), "", true);
+						
+						emb.addField("" + '\u200b', "" + '\u200b', true);
 							
 						emb.addField(items.get(i + 8), "", true);
 						emb.addField(items.get(i + 9), "", true);
