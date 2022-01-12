e57e6e83b7892b9f49483fdb46c8453ac3f07eda
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-01-12 12:30:56 -0600

    leaderboard rework

--- src/main/java/commands/BuildLeaderboard.java
+++ src/main/java/commands/BuildLeaderboard.java
@@ -52,7 +52,7 @@ public class BuildLeaderboard extends EmbedPaginator.Builder {
 				Guild guild = NonAPICommands.pubGuild;
 				try {
 					if (guild.getMemberById(rs.getString("id")).getUser().getAsTag().length() > 13)
-						items.add(guild.getMemberById(rs.getString("id")).getUser().getAsTag().substring(0, 14) + "");
+						items.add(guild.getMemberById(rs.getString("id")).getUser().getAsTag().substring(0, 10) + "");
 					else
 						items.add(guild.getMemberById(rs.getString("id")).getUser().getAsTag());
 				} catch (NullPointerException e) {
