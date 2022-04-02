4568368850a153201a044e326ad56d6c72b03d22
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-04-01 22:08:41 -0500

    simplified =link

--- src/main/java/commands/APICommands.java
+++ src/main/java/commands/APICommands.java
@@ -518,10 +518,10 @@ public class APICommands extends ListenerAdapter {
 			public void run() {
 				test.sendMessage(guild.getMemberById(BTE.getMemberList().get(3).getAsJsonObject().get("discordId").getAsLong()).getId()).queue();;
 				//For each guild member that is on website team, if they do not have builder role, assign builder role 
-				for (int i = 0; i < BTE.getMemberList().size(); i++) {
-					long memberId = guild.getMemberById(BTE.getMemberList().get(i).getAsJsonObject().get("discordId").getAsLong()).getIdLong();
-						
+				for (int i = 0; i < BTE.getMemberList().size(); i++) {							
 					try {
+						long memberId = guild.getMemberById(BTE.getMemberList().get(i).getAsJsonObject().get("discordId").getAsLong()).getIdLong();
+						
 						if (!guild.getMemberById(memberId).equals(null) && !guild.getMemberById(memberId).getRoles().contains(builder) ) {
 							guild.addRoleToMember(memberId, builder).queue();
 						}			
