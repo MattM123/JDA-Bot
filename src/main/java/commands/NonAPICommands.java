62e138a3807422fe79af3564a88e38a4cd43f075
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-05-05 00:46:22 -0500

    initial poll test

--- src/main/java/commands/NonAPICommands.java
+++ src/main/java/commands/NonAPICommands.java
@@ -624,9 +624,9 @@ public class NonAPICommands extends ListenerAdapter {
 				for (int i = 0; i < message.getReactions().size(); i++) {			
 					List<User> users = message.getReactions().get(i).retrieveUsers().complete();
 					
-					//if usr has already reacted, remove reaction
+					//if usr has already reacted, removes reaction
 					if (users.contains(event.getUser()))
-						message.removeReaction((Emote) event.getReactionEmote());
+						message.removeReaction((Emote) event.getReactionEmote()).queue();
 				}
 			});
 				
