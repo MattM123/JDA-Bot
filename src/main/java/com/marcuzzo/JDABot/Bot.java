40575e9e9f28b549de95fdf7e982b062d62a6a9f
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-05-24 13:35:37 -0500

    slash commands conversion

--- src/main/java/com/marcuzzo/JDABot/Bot.java
+++ src/main/java/com/marcuzzo/JDABot/Bot.java
@@ -32,20 +32,22 @@ public class Bot {
 			
 			Guild guild = jda.getGuildById(735990134583066679L);
 			
-		//	if (guild != null) {
+			if (guild != null) {
 		
 				guild.upsertCommand(new CommandData("console", "Send a console command to the BTE Midwest minecraft server")
 	                    .addOption(OptionType.STRING, "command", "The command to send to the server console")).queue();
 				
 				guild.upsertCommand(new CommandData("apply", "Obtain trial builder permissions on the Minecraft server")).queue();
 				
-				guild.upsertCommand(new CommandData("server", "View resource usage of the Minecraft server"));
+				guild.upsertCommand(new CommandData("server", "View resource usage of the Minecraft server")).queue();
 				
 				guild.upsertCommand(new CommandData("getapp", "View an application of a user who has previously applied to the team")
 						.addOption(OptionType.USER, "user", "The user whos applications you want to retrieve")
-						.addOption(OptionType.INTEGER, "n", "Retrieve the n-th application from the users application history"));
+						.addOption(OptionType.INTEGER, "n", "Retrieve the n-th application from the users application history")).queue();
+				
+				guild.updateCommands().addCommands(cmds).queue();
 			
-			//}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
