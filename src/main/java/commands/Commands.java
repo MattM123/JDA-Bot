37c09a8af7554efd22f35ec9c233a122bf7e7404
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-05-27 09:23:36 -0500

    onReady separation

--- src/main/java/commands/Commands.java
+++ src/main/java/commands/Commands.java
@@ -1,45 +1,23 @@
 package commands;
 
 import java.awt.Color;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.function.Consumer;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
-import com.marcuzzo.JDABot.Bot;
 import com.mattmalec.pterodactyl4j.DataType;
 import com.mattmalec.pterodactyl4j.PteroBuilder;
 import com.mattmalec.pterodactyl4j.client.entities.ClientServer;
 import com.mattmalec.pterodactyl4j.client.entities.PteroClient;
 
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.InteractionHook;
-import net.dv8tion.jda.api.interactions.commands.Command;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
-import net.dv8tion.jda.api.interactions.commands.privileges.CommandPrivilege;
 import net.dv8tion.jda.api.requests.RestAction;
-import net.dv8tion.jda.internal.JDAImpl;
 
 
 public class Commands extends ListenerAdapter {
@@ -50,9 +28,6 @@ public class Commands extends ListenerAdapter {
 	
 	//The minecraft server thats represented by a Ptero API instance
 	private ClientServer midwestServer = pteroAPI.retrieveServerByIdentifier(System.getenv("SERVER_ID")).execute();
-	
-	//Guild used for onReady Events
-	private Guild guild;
 	
 	@Override
 	public void onSlashCommand(SlashCommandEvent event) {
@@ -247,110 +222,7 @@ public class Commands extends ListenerAdapter {
 				event.reply("Only staff can execute this command").queue();
 		}
 	}
-
-	@Override
-	public void onReady(ReadyEvent event) {
-		Timer appTimer = new Timer();
-		Timer permTimer = new Timer();
-		guild = Bot.jda.getGuildById(735990134583066679L);
-		Role builder = guild.getRoleById(735991952931160104L);
-		
-//-------------------------------------------------------------------------------------------------------------------------------------------	
-//Notifies staff members of new applications since BTE bot stopped doing it
-		appTimer.scheduleAtFixedRate(new TimerTask() {
-			@Override
-			public void run() {
-				TextChannel staff = event.getJDA().getGuildById(735990134583066679L).getTextChannelById(951957461869420565L);									
-				EmbedBuilder emb = new EmbedBuilder();
-				emb.setColor(Color.blue);
-				
-				//if a message already exists in the channel, edits the current one to include the updated pending applications if there are any
-				try {
-					if (staff.hasLatestMessage()) {
-						RestAction<Message> action = staff.retrieveMessageById(staff.getLatestMessageId());
-						Message message = action.complete();
-						
-						if (BTE.getPendingApplications() != null && BTE.getPendingApplications().getApplications().size() > 0) {						    							    	
-						   	for (int i = 0; i < BTE.getPendingApplications().getApplications().size(); i++) {
-						   		emb.addField(BTE.getPendingApplications().getApplications().get(i).user.getUserTag() + " has applied to the team.\n" ,
-					   				"View their application here: https://buildtheearth.net/buildteams/36/applications/" 
-				    				+ BTE.getPendingApplications().getApplications().get(i).id + "\n\n", false);			
-					    	}
-					    	message.editMessageEmbeds(emb.build()).queue();
-					    }
-					    //deletes message if all apps have already been reviewed
-					    else {
-					    	message.delete().queue();
-					    }
-					}
-					//If there is no message history, a message is created and sent instead of edited if there is at least one pending application
-				} catch (ErrorResponseException e) {
-					try {
-						if (BTE.getPendingApplications().getApplications().size() > 0) {    	
-							   for (int i = 0; i < BTE.getPendingApplications().getApplications().size(); i++) {
-						    	emb.addField(BTE.getPendingApplications().getApplications().get(i).user.getUserTag() + " has applied to the team.\n" ,
-						   				"View their application here: https://buildtheearth.net/buildteams/36/applications/" 
-						    			+ BTE.getPendingApplications().getApplications().get(i).id + "\n\n", false);	
-							   }
-							   	staff.sendMessageEmbeds(emb.build()).queue();	    
-						}
-					} catch (NullPointerException r) {
-						r.printStackTrace();
-					}
-					//There is a message history but the message was not sent by the bot send new message instead of trying to edit
-				} catch (IllegalStateException e) {
-					if (BTE.getPendingApplications().getApplications().size() > 0) {    	
-						   for (int i = 0; i < BTE.getPendingApplications().getApplications().size(); i++) {
-					    	emb.addField(BTE.getPendingApplications().getApplications().get(i).user.getUserTag() + " has applied to the team.\n" ,
-					   				"View their application here: https://buildtheearth.net/buildteams/36/applications/" 
-					    			+ BTE.getPendingApplications().getApplications().get(i).id + "\n\n", false);	
-						   }
-						   	staff.sendMessageEmbeds(emb.build()).queue();	    
-					}
-					
-				} catch (NullPointerException e) {
-					System.out.println("Somthing was null ig idk");
-				}
-			}
-		}, 1000, 10000);
-		
-//-------------------------------------------------------------------------------------------------------------------------------------------	
-//Compares team ID list with discord users and assigns build perms if necessary
-		TextChannel test = guild.getTextChannelById(786328890280247327L);
-		permTimer.scheduleAtFixedRate(new TimerTask() {
-
-			@Override
-			public void run() {
-
-				//For each guild member that is on website team, if they do not have builder role, assign builder role 	
-				try {
-						for (int i = 0; i < BTE.getMemberList().size(); i++) {				
-							try {
-								long memberId = guild.getMemberById(BTE.getMemberList().get(i).getAsJsonObject().get("discordId").getAsLong()).getIdLong();
-								if (!guild.getMemberById(memberId).equals(null) && !guild.getMemberById(memberId).getRoles().contains(builder)) {
-									guild.addRoleToMember(memberId, builder).queue();
-								}	
-							} catch (NullPointerException e) {
-								//If discord ID does not exist in guild, skips user since role cannot be assigned
-								continue;
-							}
-						}
-				} catch (MalformedURLException e) {
-					String stack = ExceptionUtils.getStackTrace(e);
-					test.sendMessage("Malformed URL while retrieving member list <@387330197420113930>").queue();
-					test.sendMessage(stack.subSequence(0, 1990)).queue();
-				} catch (IOException e) {
-					String stack = ExceptionUtils.getStackTrace(e);
-					if (stack.contains("Server returned HTTP response code: 5"))
-						test.sendMessage("Server side IO Exception while retrieving member list\n" + e.getMessage());
-				}
-			}		
-		}, 1000, 300000);
-	}	
-	
-}	
-
-			
+}		
 /*==========================================================================================================================================
 ======================================================LEGACY: replaced with DiscordSRV==========================================================
 ============================================================================================================================================
---
+++ src/main/java/commands/OnReadyEvents.java
@@ -0,0 +1,127 @@
+package commands;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+import com.marcuzzo.JDABot.Bot;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.RestAction;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
+
+public class OnReadyEvents extends ListenerAdapter {
+
+	private BuildTheEarthAPI BTE = new BuildTheEarthAPI(System.getenv("BTE_API"));
+	private Guild guild;
+	
+	@Override
+	public void onReady(ReadyEvent event) {
+		Timer appTimer = new Timer();
+		Timer permTimer = new Timer();
+		guild = Bot.jda.getGuildById(735990134583066679L);
+		Role builder = guild.getRoleById(735991952931160104L);
+		
+//-------------------------------------------------------------------------------------------------------------------------------------------	
+//Notifies staff members of new applications since BTE bot stopped doing it
+		appTimer.scheduleAtFixedRate(new TimerTask() {
+			@Override
+			public void run() {
+				TextChannel staff = event.getJDA().getGuildById(735990134583066679L).getTextChannelById(951957461869420565L);									
+				EmbedBuilder emb = new EmbedBuilder();
+				emb.setColor(Color.blue);
+				
+				//if a message already exists in the channel, edits the current one to include the updated pending applications if there are any
+				try {
+					if (staff.hasLatestMessage()) {
+						RestAction<Message> action = staff.retrieveMessageById(staff.getLatestMessageId());
+						Message message = action.complete();
+						
+						if (BTE.getPendingApplications() != null && BTE.getPendingApplications().getApplications().size() > 0) {						    							    	
+						   	for (int i = 0; i < BTE.getPendingApplications().getApplications().size(); i++) {
+						   		emb.addField(BTE.getPendingApplications().getApplications().get(i).user.getUserTag() + " has applied to the team.\n" ,
+					   				"View their application here: https://buildtheearth.net/buildteams/36/applications/" 
+				    				+ BTE.getPendingApplications().getApplications().get(i).id + "\n\n", false);			
+					    	}
+					    	message.editMessageEmbeds(emb.build()).queue();
+					    }
+					    //deletes message if all apps have already been reviewed
+					    else {
+					    	message.delete().queue();
+					    }
+					}
+					//If there is no message history, a message is created and sent instead of edited if there is at least one pending application
+				} catch (ErrorResponseException e) {
+					try {
+						if (BTE.getPendingApplications().getApplications().size() > 0) {    	
+							   for (int i = 0; i < BTE.getPendingApplications().getApplications().size(); i++) {
+						    	emb.addField(BTE.getPendingApplications().getApplications().get(i).user.getUserTag() + " has applied to the team.\n" ,
+						   				"View their application here: https://buildtheearth.net/buildteams/36/applications/" 
+						    			+ BTE.getPendingApplications().getApplications().get(i).id + "\n\n", false);	
+							   }
+							   	staff.sendMessageEmbeds(emb.build()).queue();	    
+						}
+					} catch (NullPointerException r) {
+						r.printStackTrace();
+					}
+					//There is a message history but the message was not sent by the bot send new message instead of trying to edit
+				} catch (IllegalStateException e) {
+					if (BTE.getPendingApplications().getApplications().size() > 0) {    	
+						   for (int i = 0; i < BTE.getPendingApplications().getApplications().size(); i++) {
+					    	emb.addField(BTE.getPendingApplications().getApplications().get(i).user.getUserTag() + " has applied to the team.\n" ,
+					   				"View their application here: https://buildtheearth.net/buildteams/36/applications/" 
+					    			+ BTE.getPendingApplications().getApplications().get(i).id + "\n\n", false);	
+						   }
+						   	staff.sendMessageEmbeds(emb.build()).queue();	    
+					}
+					
+				} catch (NullPointerException e) {
+					System.out.println("Somthing was null ig idk");
+				}
+			}
+		}, 1000, 10000);
+		
+//-------------------------------------------------------------------------------------------------------------------------------------------	
+//Compares team ID list with discord users and assigns build perms if necessary
+		TextChannel test = guild.getTextChannelById(786328890280247327L);
+		permTimer.scheduleAtFixedRate(new TimerTask() {
+
+			@Override
+			public void run() {
+
+				//For each guild member that is on website team, if they do not have builder role, assign builder role 	
+				try {
+						for (int i = 0; i < BTE.getMemberList().size(); i++) {				
+							try {
+								long memberId = guild.getMemberById(BTE.getMemberList().get(i).getAsJsonObject().get("discordId").getAsLong()).getIdLong();
+								if (!guild.getMemberById(memberId).equals(null) && !guild.getMemberById(memberId).getRoles().contains(builder)) {
+									guild.addRoleToMember(memberId, builder).queue();
+								}	
+							} catch (NullPointerException e) {
+								//If discord ID does not exist in guild, skips user since role cannot be assigned
+								continue;
+							}
+						}
+				} catch (MalformedURLException e) {
+					String stack = ExceptionUtils.getStackTrace(e);
+					test.sendMessage("Malformed URL while retrieving member list <@387330197420113930>").queue();
+					test.sendMessage(stack.subSequence(0, 1990)).queue();
+				} catch (IOException e) {
+					String stack = ExceptionUtils.getStackTrace(e);
+					if (stack.contains("Server returned HTTP response code: 5"))
+						test.sendMessage("Server side IO Exception while retrieving member list\n" + e.getMessage());
+				}
+			}		
+		}, 1000, 300000);
+	}	
+}	
+
