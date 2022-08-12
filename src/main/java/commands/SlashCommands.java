578aa15392dd27c64a7403016b16deec36370087
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-08-12 10:09:36 -0500

    testing

--- src/main/java/commands/SlashCommands.java
+++ src/main/java/commands/SlashCommands.java
@@ -302,8 +302,8 @@ public class SlashCommands extends ListenerAdapter {
 			
 	        for (File textureFile : new File("src/main/java/Resources/textures/blocks/").listFiles()) {
 	            try {
-	                BufferedImage image = ImageIO.read(textureFile);
-
+	                BufferedImage image = ImageIO.read(textureFile.getAbsoluteFile());
+	                event.reply(textureFile.getAbsolutePath()).queue();
 
 	                int sumR = 0, sumG = 0, sumB = 0;
 	                for (int x = 0; x < image.getWidth(); x++) {
