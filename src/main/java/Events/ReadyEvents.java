c551879beaca02035565ea40f0b03633c8d0a926
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-10-12 11:51:11 -0500

    channel spam detection test

--- src/main/java/Events/ReadyEvents.java
+++ src/main/java/Events/ReadyEvents.java
@@ -1,61 +1,81 @@
 package Events;
 
-import java.io.BufferedWriter;
+import java.awt.List;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import com.marcuzzo.JDABot.Bot;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class ReadyEvents extends ListenerAdapter {
 	
+	class Tuple { 
+	    private Message x;
+	    private User y;
+	    
+	    public Tuple(Message x, User y) {
+	    	this.x = x;
+	    	this.y = y;
+	    }
+	    
+	    public Message getMessage() {
+	    	return x;
+	    }
+	    
+	    public User getUser() {
+	    	return y;
+	    }
+	} 
+	
 	private Guild guild = Bot.jda.getGuildById(735990134583066679L);
+	ArrayList<Tuple> messages = new ArrayList<Tuple>();
 	
-	public void onReadyEvent(ReadyEvent event) {
-		Role improve = guild.getRoleById(1006335736695500801L);
-		File file = new File("/JDABot/src/main/java/commands/RejectedUsers.txt");
-		StringBuilder newContents = new StringBuilder();
-	/*	
-		//Checks if a rejected user can re-apply, removes role if the time requirement has been met
-		try {
-			Scanner scan = new Scanner(file);
-			String line;
-			while (scan.hasNextLine()) {
-				line = scan.nextLine();
-				
-				if (line.contains(System.currentTimeMillis() + "")) {
-					guild.removeRoleFromMember(line.split(":")[0], improve).queue();
-					continue;
+	//Channel spam detection
+	public void onReadyEvent(MessageReceivedEvent event) {
+		
+		//The time interval the messages need to be sent within for it to be considered channel spam
+		int interval = 10000;
+		//The amount of messages to be considered channel spam
+		int messageAmount = 3;		
+		//Clears cache before next exection
+		messages.clear();
+		
+		long start = System.currentTimeMillis();
+		long end = start + interval;
+		
+		while (System.currentTimeMillis() != end) {
+				//Comapares cached messages and authors with new messages. Clears cache if tuples differ.
+				if (messages.isEmpty() || !messages.contains(new Tuple(event.getMessage(), event.getAuthor()))) {
+					messages.clear();
+					messages.add(new Tuple(event.getMessage(), event.getAuthor()));
 				}
-				newContents.append(line);
-			}
-			
-			//Delete File Content
-			PrintWriter pw = new PrintWriter(file);
-			pw.close();
-	
-			//Write new contents to file
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
-			writer.append(newContents.toString());
-			writer.close();		
-			
-			scan.close();	
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				//If message and author match, caches 
+				else if (messages.contains(new Tuple(event.getMessage(), event.getAuthor()))) {				
+					messages.add(new Tuple(event.getMessage(), event.getAuthor()));
+				}
+				
+				//If cache is full, user has sent 3 of the same messages in 3 different channels
+				if (messages.size() == messageAmount) {
+					
+				}
+				guild.getTextChannelById(786328890280247327L).sendMessage("Cache: " + messages.toString());
 		}
-		*/
 	}
 }
