a89536ff9b9f628d145929dbf1bc919b47876dfb
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-10-12 12:26:19 -0500

    channel spam detection test

--- src/main/java/Events/ReadyEvents.java
+++ src/main/java/Events/ReadyEvents.java
@@ -5,6 +5,7 @@ import com.marcuzzo.JDABot.Bot;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -14,10 +15,12 @@ public class ReadyEvents extends ListenerAdapter {
 	class Tuple { 
 	    private Message x;
 	    private User y;
+	    private TextChannel z;
 	    
-	    public Tuple(Message x, User y) {
+	    public Tuple(Message x, User y, TextChannel z) {
 	    	this.x = x;
 	    	this.y = y;
+	    	this.z = z;
 	    }
 	    
 	    public Message getMessage() {
@@ -26,6 +29,10 @@ public class ReadyEvents extends ListenerAdapter {
 	    
 	    public User getUser() {
 	    	return y;
+	    }
+	    
+	    public TextChannel getChannel() {
+	    	return z;
 	    }
 	} 
 	
@@ -45,16 +52,21 @@ public class ReadyEvents extends ListenerAdapter {
 		long start = System.currentTimeMillis();
 		long end = start + interval;
 		guild.getTextChannelById(786328890280247327L).sendMessage("test").queue();
-		if (event.isFromGuild()) {
+		if (event.isFromGuild() && event.getChannelType().isMessage()) {
 			while (System.currentTimeMillis() != end) {
-					//Comapares cached messages and authors with new messages. Clears cache if tuples differ.
-					if (messages.isEmpty() || !messages.contains(new Tuple(event.getMessage(), event.getAuthor()))) {
-						messages.clear();
-						messages.add(new Tuple(event.getMessage(), event.getAuthor()));
+					//Comapares cached messages and authors with new messages. 
+					//Cache only stores object if the message and author are the same but the channel is different
+					if (!messages.isEmpty()) {
+						for (int i = 0; i < messages.size(); i++) {
+							if (event.getMessage().equals(messages.get(i).getMessage()) 
+									&& event.getAuthor().equals(messages.get(i).getUser()) 
+									&& !event.getChannel().equals(messages.get(i).getChannel())) {
+								messages.add(new Tuple(event.getMessage(), event.getAuthor(), (TextChannel) event.getChannel()));
+							}
+						}
 					}
-					//If message and author match, caches 
-					else if (messages.contains(new Tuple(event.getMessage(), event.getAuthor()))) {				
-						messages.add(new Tuple(event.getMessage(), event.getAuthor()));
+					else {
+						messages.add(new Tuple(event.getMessage(), event.getAuthor(), (TextChannel) event.getChannel()));
 					}
 					
 					//If cache is full, user has sent 3 of the same messages in 3 different channels
