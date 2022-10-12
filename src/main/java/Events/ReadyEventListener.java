4acf47e7fdf6ab435ecb1352811cae02ceea0ee8
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-10-12 17:05:29 -0500

    channel spam detection test

--- src/main/java/Events/ReadyEventListener.java
+++ src/main/java/Events/ReadyEventListener.java
@@ -1,6 +1,9 @@
 package Events;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import com.marcuzzo.JDABot.Bot;
 
 import net.dv8tion.jda.api.entities.Guild;
@@ -50,7 +53,7 @@ public class ReadyEventListener extends ListenerAdapter {
 	} 
 	
 	private Guild guild = Bot.jda.getGuildById(735990134583066679L);
-	private ArrayList<Tuple> messages = new ArrayList<Tuple>();
+	private LinkedList<Tuple> messages = new LinkedList<Tuple>();
 	
 	//Channel spam detection
 	@Override
@@ -61,24 +64,35 @@ public class ReadyEventListener extends ListenerAdapter {
 	
 		//The amount of messages to be considered channel spam
 		int messageAmount = 3;		
+		
+		//How many messages should be analysed to determine spam
+		int cacheSize = 5;
 	
 		if (event.isFromGuild() && event.getChannelType().isMessage() && !event.getMessage().isEphemeral() && !event.getAuthor().isBot()) {
 				//Comapares cached messages and authors with new messages. 				
-				if (!messages.isEmpty()) {
+				if (messages.size() == cacheSize) {
+					int counter = 0;
+					User spammer = null;
 					for (int i = 0; i < messages.size(); i++) {
 						//Cache stores tuples with similar content and author but differing channels
 						//Messages need to be sent within the time interval to be tracked
 						if (event.getMessage().equals(messages.get(i).getMessage()) 
 								&& event.getAuthor().equals(messages.get(i).getUser()) 
 								&& !event.getChannel().equals(messages.get(i).getChannel())) {
-							messages.add(new Tuple(event.getMessage(), event.getAuthor(), (TextChannel) event.getChannel(), System.currentTimeMillis()));
+							counter++;
+							spammer = event.getAuthor();
 						}
 						//If cache is not full and a message with different content and author is recieved, 
-						//clears cache and stores the different message thats been recieved.
+						//resets counter to 0
 						else {
-							messages.clear();
-							messages.add(new Tuple(event.getMessage(), event.getAuthor(), (TextChannel) event.getChannel(), System.currentTimeMillis()));
+							counter = 0;
+							messages.removeLast();
+							messages.addFirst(new Tuple(event.getMessage(), event.getAuthor(), (TextChannel) event.getChannel(), System.currentTimeMillis()));
 						}
+					}
+					if (counter >= messageAmount && messages.get(0).getTime() - messages.get(messages.size()).getTime() < interval) {
+						guild.getTextChannelById(786328890280247327L).sendMessage("Channel Spammed by :\n" + spammer.getAsTag() + " in " 
+								+ (messages.get(0).getTime() - messages.get(messages.size()).getTime()) + "ms").queue();
 					}
 				}
 				//If cache is empty on message recieved, caches the message regardless of content, author, or channel to be used 
@@ -89,7 +103,7 @@ public class ReadyEventListener extends ListenerAdapter {
 					
 				//If cache is full and user has sent 3 of the same messages in 3 different channels within the time interval
 				//it is considered channel spam
-				if (messages.size() == messageAmount && messages.get(0).getTime() - messages.get(messages.size()).getTime() < interval) {				
+				if (messages.size() == cacheSize && messages.get(0).getTime() - messages.get(messages.size()).getTime() < interval) {				
 					guild.getTextChannelById(786328890280247327L).sendMessage("Channel Spammed:\n" + messages.toString()).queue();
 				}
 				guild.getTextChannelById(786328890280247327L).sendMessage("Cache: " + messages.toString()).queue();
