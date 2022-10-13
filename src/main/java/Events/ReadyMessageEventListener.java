d83c5d9ec12df51725ea6dc16404039652bd8039
Matthew Marcuzzo <mattmarcuzzo@gmail.com>
2022-10-13 10:37:14 -0500

    channel spam detection test

--- src/main/java/Events/ReadyMessageEventListener.java
+++ src/main/java/Events/ReadyMessageEventListener.java
@@ -84,11 +84,11 @@ public class ReadyMessageEventListener extends ListenerAdapter {
 								&& event.getAuthor().equals(messageCache.get(i).getUser()) 
 								&& !event.getChannel().equals(messageCache.get(i).getChannel())) {
 							counter++;
-							guild.getTextChannelById(786328890280247327L).sendMessage("c:" + counter).queue();	
+						//	guild.getTextChannelById(786328890280247327L).sendMessage("c:" + counter).queue();	
 							spammer = event.getAuthor();
 						}
 						else {
-							guild.getTextChannelById(786328890280247327L).sendMessage("v:").queue();	
+						//	guild.getTextChannelById(786328890280247327L).sendMessage("v:").queue();	
 						}
 				//		guild.getTextChannelById(786328890280247327L).sendMessage(event.getMessage().getContentRaw() + " : " + messageCache.get(i).getMessage().getContentRaw()).queue();
 					//	guild.getTextChannelById(786328890280247327L).sendMessage(event.getMessage().getAuthor().getName() + " : " + messageCache.get(i).getUser().getName()).queue();
